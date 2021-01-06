62b73799ab733709c06ccbc6d042dc2b658af1bc
Anay Joshi <anayjoshi1608@gmail.com>
2021-01-06 22:22:11 +0530

    Setting up Category Model & registering it on admin

--- ecom/api/category/models.py
+++ ecom/api/category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50) # created a column in cateory table
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True) # created_at is a one time thing
+    updated_at = models.DateTimeField(auto_now=True)
