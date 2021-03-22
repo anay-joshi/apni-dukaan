5bc402ef0d0c57d945028a3d671cc343582ed98e
Anay Joshi <anayjoshi1608@gmail.com>
2021-03-22 22:25:20 +0530

    Image serialization & Adding Product views

--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
 # Create your views here.
+from .serializers import ProductSerializer
+
+from .models import Product
+
+class ProductViewSet(viewsets.ModelViewset):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
