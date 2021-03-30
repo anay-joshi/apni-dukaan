a53c4fc67e8b60b30fdeefb1f50beb2139ffbf9b
Anay Joshi <anayjoshi1608@gmail.com>
2021-03-30 21:48:06 +0530

    Generating token in django

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import random
+
+
+def generate_session_token(length=10):
+
+    # 1st loop for lower case alpha , 2nd loop = numbers , 3rd loop for genearting 10 digit token
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)])for _ in range(length))
