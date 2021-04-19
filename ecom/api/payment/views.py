0a53cbec6cee0dd18671e5c19f279a8242049a6b
Anay Joshi <anayjoshi1608@gmail.com>
2021-04-19 18:26:06 +0530

    Generated session token for payment gateway.

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+
+
 # Create your views here.
+import braintree
+
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        braintree.Environment.Sandbox,
+        merchant_id="rmnznyw9vcmc94qn",
+        public_key="bzvgsp65pqzmspk4",
+        private_key="a2f3048b124a4436643c149136065122"
+    )
+)
+
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist():
+        return False
+
+
+@csrf_exempt
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, please login again!'})
+    return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
