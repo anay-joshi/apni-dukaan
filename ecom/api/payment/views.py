c534fb3e100758489219c64dddf4a882cf83f0a2
Anay Joshi <anayjoshi1608@gmail.com>
2021-04-20 17:21:54 +0530

    Processing the payments in backend using nonce.

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -36,3 +36,31 @@ def generate_token(request, id, token):
     if not validate_user_session(id, token):
         return JsonResponse({'error': 'Invalid session, please login again!'})
     return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
+
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, please login again!'})
+
+    nonce_from_the_client = request.POST["paymentMethodNonce"]
+    amount_from_the_client = request.POST["amount"]
+
+    result = gateway.transaction.sale({
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({
+            "success": result.is_success,
+            "transaction": {
+                'id': result.transaction.id,
+                'amount': result.transaction.amount
+            }
+        })
+    else:
+        return JsonResponse({'error': True, 'success': False})
