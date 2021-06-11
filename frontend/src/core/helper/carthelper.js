c323991c719ffa2c0255e907adf8519eca107948
Anay Joshi <anayjoshi1608@gmail.com>
2021-06-11 17:56:19 +0530

    Loading & removing item from cart

--- frontend/src/core/helper/carthelper.js
+++ frontend/src/core/helper/carthelper.js
@@ -13,3 +13,38 @@ export const addItemToCart = (item, next) => {
     next();
   }
 };
+
+export const loadCart = () => {
+  if (typeof window !== undefined) {
+    if (localStorage.getItem("cart")) {
+      return JSON.parse(localStorage.getItem("cart"));
+    }
+  }
+};
+
+export const removeItemFromCart = (productId) => {
+  let cart = [];
+  if (typeof window !== undefined) {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+
+    cart.map((product, i) => {
+      if (product.id === productId) {
+        cart.splice(i, 1);
+      }
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  return cart;
+};
+
+export const cartEmpty = (next) => {
+  if (typeof window !== undefined) {
+    localStorage.removeItem("cart");
+    let cart = [];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    next();
+  }
+};
