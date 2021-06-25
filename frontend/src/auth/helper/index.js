d0057629653029c7a2ed73457ad11ec09c39b729
Anay Joshi <anayjoshi1608@gmail.com>
2021-06-25 21:58:25 +0530

    Signout and authenticating user handled

--- frontend/src/auth/helper/index.js
+++ frontend/src/auth/helper/index.js
@@ -32,3 +32,41 @@ export const signin = (uesr) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const authenticate = (data, next) => {
+  if (typeof window !== undefined) {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+export const isAuthenticated = () => {
+  if (typeof window !== undefined) {
+    return false;
+  }
+
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
+
+export const signout = (next) => {
+  const userId = isAuthenticated() && isAuthenticated().user.id;
+
+  if (typeof window !== undefined) {
+    localStorage.removeItem("jwt");
+    cartEmpty(() => {});
+    //next();
+
+    return fetch(`${API}user/logout/${userId}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        console.log("Signing out");
+        next();
+      })
+      .catch((err) => console.log(err));
+  }
+};
