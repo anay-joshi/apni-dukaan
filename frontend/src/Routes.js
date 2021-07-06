43223ad5a225f9ce3abfe8b459fc0d9ff0c0674c
Anay Joshi <anayjoshi1608@gmail.com>
2021-07-06 22:08:39 +0530

    Signup Page loaded in the route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import Home from "./core/Home";
 
@@ -8,6 +9,7 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
         {/* <Route path="/user/dashboard" exact component={} /> */}
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/user/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Base from "../core/Base";
+
+const Signup = () => {
+  return (
+    <Base title="Sign up Page" description="A signup for apni dukan user">
+      <p> Test of Signup Page</p>
+    </Base>
+  );
+};
+
+export default Signup;
