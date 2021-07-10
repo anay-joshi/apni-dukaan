7ad54f6590cd8179334c1518380187c62108ea67
Anay Joshi <anayjoshi1608@gmail.com>
2021-07-10 22:47:18 +0530

    Basic nav bar structure developed

---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-dark">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Signin
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
