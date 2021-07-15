1f2084afb4b98daf46a8d284e4f6c87948dd6573
Anay Joshi <anayjoshi1608@gmail.com>
2021-07-15 22:23:28 +0530

    Loaded products in cart page & resolved some bugs

--- frontend/src/core/Cart.js
+++ frontend/src/core/Cart.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Base from "./Base";
+import Card from "./Card";
+import { loadCart } from "./helper/carthelper";
 
 const Cart = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, []);
+
+  const loadAllProducts = (products) => {
+    return (
+      <div>
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            removeFromCart={true}
+            addToCart={false}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const loadAllCheckout = () => {
+    return (
+      <div>
+        <h1>Checkout</h1>
+      </div>
+    );
+  };
+
   return (
     <Base title="Cart Page" description="Welcome to Checkout">
-      <h1>Cart Page</h1>
+      <div className="row text-center">
+        <div className="col-6">{loadAllProducts(products)}</div>
+        <div className="col-6">{loadAllCheckout()}</div>
+      </div>
     </Base>
   );
 };
